70a2dd4e66314d86367485d9bcb0aa58f38cf6bb
Frank Fella <Frank@TechElevator.com>
2021-06-29 19:40:37 -0400

    Instructor Push 06/29/2021 19:40:37

---
+++ Module-2-Capstone/java/tenmo-server/src/main/java/com/techelevator/tenmo/controller/ApiController.java
@@ -0,0 +1,12 @@
+package com.techelevator.tenmo.controller;
+
+/*******************************************************************************************************
+ * This is where you code any API controllers you may create
+ *
+ * Feel free to create additional controller classs if you would
+ * like to have separate controller classses base on functionality or use
+********************************************************************************************************/
+
+public class ApiController {
+
+}
